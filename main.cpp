bc901e3114c867530e7389e50827a1128cd64e42
GustavoPAraujo <araujops.gustavo@gmail.com>
2025-04-28 20:06:54 -0300

    commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 const int trigPin = D5;
 const int echoPin = D2;
+const int redLed = D7;
+const int greenLed = D9;
+
 
 void setup() {
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  digitalWrite(greenLed, HIGH);
   digitalWrite(trigPin, LOW);
 }
 
@@ -25,12 +31,20 @@ void loop() {
 
   if (dur == 0) {
     Serial.println("Sem pulso detectado");
+    return;
+  }
+  float dist = dur * 0.034 / 2.0;
+  Serial.print("Dist: ");
+  Serial.print(dist);
+  Serial.println(" cm");
+
+  if (dist <= 20) {
+    digitalWrite(greenLed, LOW);
+    digitalWrite(redLed, HIGH);
   } else {
-    float dist = dur * 0.034 / 2.0;
-    Serial.print("Dist: ");
-    Serial.print(dist);
-    Serial.println(" cm");
+    digitalWrite(greenLed, LOW);
+    digitalWrite(redLed, HIGH);
   }
 
-  delay(200);
+  delay(1000);
 }
